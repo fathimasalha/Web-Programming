1c161804b91978d83bc122b27d7e6d14bdcad700
Fathima Salha NK <116252373+fathimasalha@users.noreply.github.com>
2023-01-19 13:03:58 +0530

    Add files via upload

---
+++ php/print.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<?php
+echo"<h2>PHP is fun</h2>";
+echo"hello world<br>";
+echo"I'm about to learn PHP!<br>";
+?>
+</body>
+</html>
---
+++ php/sum.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<h1>Sum of 3 numbers</h1>
+
+<?php
+$a=5;
+$b=10;
+$c=20;
+$z=$a+$b+$c;
+echo "sum=",$z;
+?>
+
+</body>
+</html>
